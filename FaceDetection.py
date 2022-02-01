688d76972ca0f1db698750a4d980d8d511095446
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-02-01 08:33:43 +0530

    Added more

---
+++ FaceDetection.py
@@ -0,0 +1,7 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('Resources/photo.jpg')
+
+cv2.imshow("Result", img)
+cv2.waitKey(0)
--- ShapeDetection.py
+++ ShapeDetection.py
@@ -1,23 +1,26 @@
 import cv2
+import numpy
 import numpy as np
 
-def stackImages(scale,imgArray):
+
+def stackImages(scale, imgArray):
     rows = len(imgArray)
     cols = len(imgArray[0])
     rowsAvailable = isinstance(imgArray[0], list)
     width = imgArray[0][0].shape[1]
     height = imgArray[0][0].shape[0]
     if rowsAvailable:
-        for x in range ( 0, rows):
+        for x in range(0, rows):
             for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
                     imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
                 else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]),
+                                                None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
         imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
+        hor = [imageBlank] * rows
+        hor_con = [imageBlank] * rows
         for x in range(0, rows):
             hor[x] = np.hstack(imgArray[x])
         ver = np.vstack(hor)
@@ -26,19 +29,58 @@ def stackImages(scale,imgArray):
             if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
                 imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
             else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
             if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
+        hor = np.hstack(imgArray)
         ver = hor
     return ver
 
-img = cv2.imread('Resources/shapes.png')
 
+def getContours(img):
+    countours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for cnt in countours:
+        area = cv2.contourArea(cnt)
+        print(area)
+        if area > 500:
+            cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
+            perimeter = cv2.arcLength(cnt, True)
+            # print(perimeter)
+            approx = cv2.approxPolyDP(cnt, 0.02 * perimeter, True)
+            print(len(approx))
+            objCor = len(approx)
+            x, y, w, h = cv2.boundingRect(approx)
+
+            if objCor == 3:
+                objectType = "Tri"
+            elif objCor == 4:
+                aspectRatio = w / float(h)
+                if aspectRatio > 0.95 and aspectRatio < 1.05:
+                    objectType = "Square"
+                else:
+                    objectType = "Rectangle"
+            elif objCor > 4:
+                objectType = "Circles"
+            else:
+                objectType = "None"
+
+            cv2.rectangle(imgContour, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(imgContour, objectType, (x + (w // 2) - 10, y + (h // 2) - 10), cv2.FONT_HERSHEY_PLAIN, 0.9,
+                        (0, 0, 0), 2)
+
+
+img = cv2.imread('Resources/shapes.png')
+imgContour = img.copy()
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 1)
 
-imgFinal  = stackImages(0.3, ([img , imgGray , imgBlur]))
+imgCanny = cv2.Canny(imgBlur, 50, 50)
+getContours(imgCanny)
+imgBlank = numpy.zeros_like(img)
+imgFinal = stackImages(0.6, ([img, imgGray, imgBlur], [imgCanny, imgContour, imgBlank]))
 # cv2.imshow("Original" , img);
 # cv2.imshow("grey" , imgGray);
 # cv2.imshow("blur" , imgBlur);
+
+cv2.imshow("ImageFinal", imgFinal)
+cv2.waitKey(0)
